75195f3a8c53d0baab218b6bfd430f64018be432
JohnWu1111 <johnwu@sjtu.edu.cn>
2021-07-19 19:51:55 +0800

    Update P6.m
    all bug fixed

--- P6.m
+++ P6.m
@@ -80,21 +80,39 @@ for k = 1:len_s
     for i = 1:L-1
         j = i+1;
         if phi_k(i) == phi_k(j)
-            H3(k,k) = U/4;
+            H3(k,k) = H3(k,k) + U/4;
         else
-            H3(k,k) = -U/4;
+            H3(k,k) = H3(k,k) - U/4;
             phi_b = phi_k;
             phi_b(j) = phi_k(i);
             phi_b(i) = phi_k(j);
             b = twototen(phi_b);
             b = store2(b);
-            H3(k,b) = -J/2;
+            H3(k,b) = H3(k,b) - J/2;
         end
+    end
+    if phi_k(1) == phi_k(8)
+        H3(k,k) = H3(k,k) + U/4;
+    else
+        H3(k,k) = H3(k,k) - U/4;
+        phi_b = phi_k;
+        phi_b(1) = phi_k(8);
+        phi_b(8) = phi_k(1);
+        b = twototen(phi_b);
+        b = store2(b);
+        H3(k,b) = H3(k,b) + J/2;
     end
 end
 
 e2 = eig(H3);
 e2(1)
+
+check = zeros(len,1);
+for i = 1:len
+    if twototen(tentotwo(i,L)) == i
+        check(i) = 1;
+    end
+end
 
 function y = twototen(phi)
     len = length(phi);
